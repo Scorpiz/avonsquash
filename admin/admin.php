410fa7bd8dc8abd11f794500bf5a09dbf1850ed0
NOceane <32477116+NOceane@users.noreply.github.com>
2018-07-23 15:40:44 +0200

    1

--- admin/admin.php
+++ admin/admin.php
@@ -1,27 +1,190 @@
 <?php
 session_start();
-include "header.php";
 if(isset($_SESSION['connecte']) && $_SESSION['connecte'] == true){
     ?>
-
-    <nav class="navbar  bg-light">
-        <!-- Links -->
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" href="logout.php">Retour</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link 2</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link 3</a>
-            </li>
-        </ul>
-
-
     <?php
 } else {
     header("Location:login.php");
 }
-include "footer.php";
 ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Bootstrap Admin Theme v3</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="Bootstrap-Admin-Theme-3-master/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- styles -->
+    <link href="Bootstrap-Admin-Theme-3-master/css/styles.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+  	<div class="header">
+	     <div class="container">
+	        <div class="row">
+	           <div class="col-md-5">
+	              <!-- Logo -->
+	              <div class="logo">
+	                 <h1><a href="index.html">Bootstrap Admin Theme</a></h1>
+	              </div>
+	           </div>
+	           <div class="col-md-5">
+	              <div class="row">
+	                <div class="col-lg-12">
+	                  <div class="input-group form">
+	                       <input type="text" class="form-control" placeholder="Search...">
+	                       <span class="input-group-btn">
+	                         <button class="btn btn-primary" type="button">Search</button>
+	                       </span>
+	                  </div>
+	                </div>
+	              </div>
+	           </div>
+	           <div class="col-md-2">
+	              <div class="navbar navbar-inverse" role="banner">
+	                  <nav class="collapse navbar-collapse bs-navbar-collapse navbar-right" role="navigation">
+	                    <ul class="nav navbar-nav">
+	                      <li class="dropdown">
+	                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">My Account <b class="caret"></b></a>
+	                        <ul class="dropdown-menu animated fadeInUp">
+	                          <li><a href="profile.html">Profile</a></li>
+	                          <li><a href="login.html">Logout</a></li>
+	                        </ul>
+	                      </li>
+	                    </ul>
+	                  </nav>
+	              </div>
+	           </div>
+	        </div>
+	     </div>
+	</div>
+
+    <div class="page-content">
+    	<div class="row">
+		  <div class="col-md-2">
+		  	<div class="sidebar content-box" style="display: block;">
+                <ul class="nav">
+                    <!-- Main menu -->
+                    <li class="current"><a href="admin.php"><i class="glyphicon glyphicon-home"></i>Accueil</a></li>
+                    <li><a href="calendar.html"><i class="glyphicon glyphicon-calendar"></i> Tarifs</a></li>
+                    <li><a href="stats.html"><i class="glyphicon glyphicon-stats"></i> Espace pro</a></li>
+                    <li><a href="tables.html"><i class="glyphicon glyphicon-list"></i> Partenaire</a></li>
+                    <li><a href="buttons.html"><i class="glyphicon glyphicon-record"></i> Admin</a></li>
+                  <!--  <li><a href="editors.html"><i class="glyphicon glyphicon-pencil"></i> Editors</a></li>
+                    <li><a href="forms.html"><i class="glyphicon glyphicon-tasks"></i> Forms</a></li> -->
+                  <!--  <li class="submenu">
+                         <a href="#">
+                            <i class="glyphicon glyphicon-list"></i> Pages
+                            <span class="caret pull-right"></span>
+                         </a>
+                         <!-- Sub menu -->
+                     <!--    <ul>
+                            <li><a href="login.html">Login</a></li>
+                            <li><a href="signup.html">Signup</a></li>
+                        </ul>
+                    </li>
+                </ul> --> 
+             </div>
+		  </div>
+		  <div class="col-md-10">
+		  	<div class="row">
+                <div class="col-md-6">
+		  			<div class="row">
+		  				<div class="col-md-12">
+		  					<div class="content-box-header">
+			  					<div class="panel-title">Logo </div>
+								
+								<div class="panel-options">
+									<a href="#" data-rel="collapse"><i class="glyphicon glyphicon-refresh"></i></a>
+									<a href="#" data-rel="reload"><i class="glyphicon glyphicon-cog"></i></a>
+								</div>
+				  			</div>
+				  			<div class="content-box-large box-with-header">
+                                <form method="post">
+                                    <input type="file" value="">
+                                </form>
+							</div>
+		  				</div>
+		  			</div>
+		  		</div> 
+                <!-- info agence -->
+                <?php $infos = viewAgenceInfos(); ?>
+		  		<div class="col-md-6">
+		  			<div class="row">
+		  				<div class="col-md-12">
+		  					<div class="content-box-header">
+			  					<div class="panel-title">Info Agence</div>
+								
+								<div class="panel-options">
+									<a href="#" data-rel="collapse"><i class="glyphicon glyphicon-refresh"></i></a>
+									<a href="#" data-rel="reload"><i class="glyphicon glyphicon-cog"></i></a>
+								</div>
+				  			</div>
+				  			<div class="content-box-large box-with-header">
+                                <div class="panel-body">
+		  					<table class="table table-striped">
+				              <thead>
+				                <tr>
+				                  <th>Email</th>
+				                  <th>Téléphone</th>
+				                  <th>Adresse</th>   
+				                </tr>
+				              </thead>
+				              <tbody>
+				                <tr>
+				                  <td><?= $infos['email'] ?></td>
+				                  <td><?= $infos['telephone'] ?></td>
+				                  <td><?= $infos['numero']." ".$infos['rue']."<br>".$infos['cp']." ".$infos['ville'] ?></td>
+                                  </tr>
+                                        </tbody>
+				                    </table>
+		  				        </div>
+							</div>
+		  				</div>
+		  			</div>
+		  		</div>
+		  	</div>
+
+		  	<div class="row">
+		  		<div class="col-md-12 panel-warning">
+		  			<div class="content-box-header panel-heading">
+	  					<div class="panel-title ">Photo fond Accueil</div>
+						
+						<div class="panel-options">
+							<a href="#" data-rel="collapse"><i class="glyphicon glyphicon-refresh"></i></a>
+							<a href="#" data-rel="reload"><i class="glyphicon glyphicon-cog"></i></a>
+						</div>
+		  			</div>
+		  			<div class="content-box-large box-with-header">
+			  			Pellentesque luctus quam quis consequat vulputate. Sed sit amet diam ipsum. Praesent in pellentesque diam, sit amet dignissim erat. Aliquam erat volutpat. Aenean laoreet metus leo, laoreet feugiat enim suscipit quis. Praesent mauris mauris, ornare vitae tincidunt sed, hendrerit eget augue. Nam nec vestibulum nisi, eu dignissim nulla.
+						<br /><br />
+					</div>
+		  		</div>
+		  	</div>
+		  </div>
+		</div>
+    </div>
+
+    <footer>
+         <div class="container">
+         
+            <div class="copy text-center">
+               Copyright 2014 <a href='#'>Website</a>
+            </div>
+            
+         </div>
+      </footer>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://code.jquery.com/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="Bootstrap-Admin-Theme-3-master/bootstrap/js/bootstrap.min.js"></script>
+    <script src="Bootstrap-Admin-Theme-3-master/js/custom.js"></script>
+  </body>
+</html>
---
+++ admin/functions.php
@@ -0,0 +1,29 @@
+<?php
+function viewStatus(){
+    global $bdd;
+    $req = $bdd->prepare("SELECT * FROM statut");
+    $req->execute();
+    return $req->fetchAll();
+}
+
+function viewFormule(){
+    global $bdd;
+    $req = $bdd->prepare("SELECT * FROM formule");
+    $req->execute();
+    return $req->fetchAll();
+}
+
+function viewTarif($id_f, $deletable){
+    global $bdd;
+    $req = $bdd->prepare("SELECT * FROM tarif t, statut s WHERE t.id_s = s.id_s AND  t.id_f = '".$id_f."' AND can_be_deleted = '".$deletable."' ORDER BY libelle_s");
+    $req->execute();
+    return $req->fetchAll();
+}
+
+function viewAgenceInfos(){
+    global $bdd;
+    $req = $bdd->prepare("SELECT * FROM agence");
+    $req->execute();
+    return $req->fetch();
+}
+?>
